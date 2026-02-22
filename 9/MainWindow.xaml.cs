3c1b49bfec4f0d33eebfa27f6563848cd43838f7
dima <balecored@gmail.com>
2026-02-22 14:13:20 +0500

    сделал бойню между учебными заведениями Шаповалов

--- 9/MainWindow.xaml.cs
+++ 9/MainWindow.xaml.cs
@@ -1,28 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _9
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Boets> Warriors { get; set; }
+        private Random random = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Warriors = new ObservableCollection<Boets>
+            {
+                new Bystrik(),
+                new VasyaSToporom(),
+                new Shkaf()
+            };
+
+            DataContext = this;
+        }
+
+        private void AttackButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as System.Windows.Controls.Button;
+            var target = button?.CommandParameter as Boets;
+
+            if (target == null || target.Health <= 0) return;
+
+            int damage = random.Next(10, 31);
+            int oldHealth = target.Health;
+
+            target.OyBolnlo(damage);
+
+            int actualDamage = oldHealth - target.Health;
+            LogList.Items.Add($"{target.Name} получил {actualDamage} урона. Осталось: {target.Health}");
+
+            if (target.Health <= 0)
+            {
+                LogList.Items.Add($"{target.Name} погиб!");
+            }
         }
     }
 }
---
+++ 9/Warriors.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace _9
+{
+    public class Boets : INotifyPropertyChanged
+    {
+        private int _health = 100;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get; set; }
+        public string Emoji { get; set; }
+
+        public int Health
+        {
+            get => _health;
+            set
+            {
+                _health = value > 0 ? value : 0;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(HealthText));
+            }
+        }
+
+        public string HealthText => $"{Emoji} {Health}/{(this is Shkaf ? 150 : 100)}";
+
+        public Boets(string name, string emoji)
+        {
+            Name = name;
+        }
+
+        public virtual void OyBolnlo(int damage)
+        {
+            Health -= damage;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+    public class Bystrik : Boets
+    {
+        public Bystrik() : base("Славяновка", "") { }
+
+        public override void OyBolnlo(int damage)
+        {
+            Health -= (int)(damage * 0.6);
+        }
+    }
+    public class VasyaSToporom : Boets
+    {
+        public VasyaSToporom() : base("ПКПС", "") { }
+    }
+    public class Shkaf : Boets
+    {
+        public Shkaf() : base("Авиатехникум", "")
+        {
+            Health = 150;
+        }
+
+        public override void OyBolnlo(int damage)
+        {
+            Health -= (int)(damage * 1.2);
+        }
+    }
+}
